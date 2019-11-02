a0676ae236ce19f5be66a33ecbd810e2e3be1e24
Lucas Gigliozzi <lucasg@esna.com>
2019-11-02 15:01:15 -0400

    Miscellaneous AR Projects

---
+++ AR Picture Recognition/Assets/Scripts/ResetPos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPos : MonoBehaviour
+{
+    Vector3 originalpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalpos = transform.localPosition; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collide)
+    {
+        if (collide.gameObject.CompareTag("Wall"))
+        {
+            transform.localPosition = originalpos; 
+        }
+    }
+
+}
